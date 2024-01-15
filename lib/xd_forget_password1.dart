735f43af253aad93aead0a4bf2909d2d4907b55b
Omar Elsharkawy <omarelsharkawy7@gmail.com>
2024-01-15 11:29:48 +0200

    fixing errors in xd_forget_password1.dart

--- lib/xd_forget_password1.dart
+++ lib/xd_forget_password1.dart
@@ -5,9 +5,7 @@ import './xd_forget_password2.dart';
 import 'package:adobe_xd/page_link.dart';
 
 class XDForgetPassword1 extends StatelessWidget {
-  XDForgetPassword1({
-    Key key,
-  }) : super(key: key);
+  const XDForgetPassword1({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,17 +34,17 @@ class XDForgetPassword1 extends StatelessWidget {
                     border:
                         Border.all(width: 1.5, color: const Color(0xffd3d3d3)),
                   ),
-                  margin: EdgeInsets.fromLTRB(0.0, 21.0, 0.0, 0.0),
+                  margin: const EdgeInsets.fromLTRB(0.0, 21.0, 0.0, 0.0),
                 ),
                 Pinned.fromPins(
                   Pin(size: 127.0, end: 201.0),
                   Pin(size: 17.0, middle: 0.0),
-                  child: Text(
+                  child: const Text(
                     'Enter Your Mobile No.',
                     style: TextStyle(
                       fontFamily: 'Poppins',
                       fontSize: 12,
-                      color: const Color(0xff444444),
+                      color: Color(0xff444444),
                       fontWeight: FontWeight.w500,
                       height: 1.6666666666666667,
                     ),
@@ -61,12 +59,12 @@ class XDForgetPassword1 extends StatelessWidget {
           Pinned.fromPins(
             Pin(start: 16.0, end: 15.0),
             Pin(size: 39.0, start: 72.0),
-            child: Text(
+            child: const Text(
               'We will send a code to your Mobile No. to verify\nyour Mobile No. to set the new password',
               style: TextStyle(
                 fontFamily: 'Poppins',
                 fontSize: 14,
-                color: const Color(0xff444444),
+                color: Color(0xff444444),
                 fontWeight: FontWeight.w500,
                 height: 1.3571428571428572,
               ),
@@ -101,12 +99,12 @@ class XDForgetPassword1 extends StatelessWidget {
                   Pinned.fromPins(
                     Pin(size: 78.0, middle: 0.5),
                     Pin(size: 20.0, start: 10.0),
-                    child: Text(
+                    child: const Text(
                       'Send Code',
                       style: TextStyle(
                         fontFamily: 'Poppins',
                         fontSize: 14,
-                        color: const Color(0xffffffff),
+                        color: Color(0xffffffff),
                         fontWeight: FontWeight.w500,
                       ),
                       textAlign: TextAlign.center,
